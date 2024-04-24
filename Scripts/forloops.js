55fcea6070426402449588fb47eaffeff4355383
ShanetheMorgan <144717727+ShanetheMorgan@users.noreply.github.com>
2024-04-24 18:41:00 +0000

    for loops

---
+++ Scripts/forloops.js
@@ -0,0 +1,27 @@
+console.log("Task 1")
+
+for(let i = 0; i < 11; i++) {
+    console.log(i * i);
+};
+
+console.log("Task 2")
+
+for(let i = 5; i > 0; i--) {
+    console.log(i)
+}
+console.log("Blastoff!")
+
+console.log("Task 3")
+
+for(let i = 0; i < 51; i += 2 ) {
+    console.log(i)
+}
+
+console.log("Task 4")
+
+for(let i = 0; i < 101; i++) {
+    for(let j = 0; j < 101; j++){
+        console.log(i + j)
+    }
+}
+console.log("Task 5")
