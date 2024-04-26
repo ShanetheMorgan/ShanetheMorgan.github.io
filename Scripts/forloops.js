f4c17e28f8e451319bb38d47463b9fb52b494033
ShanetheMorgan <144717727+ShanetheMorgan@users.noreply.github.com>
2024-04-26 19:50:13 +0000

    array

--- Scripts/forloops.js
+++ Scripts/forloops.js
@@ -24,7 +24,7 @@ for( let i = 2; i < 101; i++) {
 }
 console.log(sum)
 
-console.log("Task 5")
+console.log("Task 5");
 
 let fact = 1;
 for(let i = 1; i < 11; i++) {
@@ -32,3 +32,37 @@ for(let i = 1; i < 11; i++) {
     console.log(fact);
 }
 
+console.log("Task 6");
+ let arr = [3, 1, 4, 1, 5]
+ 
+ for(let i = 0; i < arr.length; i++) { 
+    console.log(arr[i])
+ }
+console.log("Task 7");
+
+for(let i = 0; i < arr.length; i++) { 
+    console.log(arr.at(-i-1))
+ }
+console.log("Task 8");
+let cubes = [];
+for(let i = 1; i < 11; i++ ) {
+    cubes.push(i**3)
+}
+console.log(cubes);
+
+console.log("Task 9");
+
+let fibarr = [];
+let prev = 0;
+let curr = 1;
+
+for(let i = 0; i < 10; i++) {
+    fibarr.push(prev)
+    let temp = curr;
+    curr = curr + prev;
+    prev = temp;
+}
+
+console.log(fibarr);
+
+console.log("Task 10");
