9292a52a371319668c140fe8edf84968bcb6f1b4
ShanetheMorgan <144717727+ShanetheMorgan@users.noreply.github.com>
2024-06-05 18:40:22 +0000

    Almost finished all pages

--- Scripts/nothing.js
+++ Scripts/nothing.js
@@ -1,3 +1,9 @@
 console.log("Hello it seems you have found the secrets hiddent withing the developer tools") ;
 
+console.log("Yes I did in fact lie to you straight to your face")
 
+for(let i = 1; i<100; i++) {
+    console.log(i)
+}
+
+console.log("You")
