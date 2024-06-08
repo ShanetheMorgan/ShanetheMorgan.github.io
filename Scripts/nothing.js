352f460884bca6738192df4cfccd0f8a1924fd68
ShanetheMorgan <144717727+ShanetheMorgan@users.noreply.github.com>
2024-06-08 23:22:13 +0000

    Tried to add dynamics

--- Scripts/nothing.js
+++ Scripts/nothing.js
@@ -6,4 +6,24 @@ for(let i = 1; i<100; i++) {
     console.log(i)
 }
 
-console.log("You")
+
+
+console.log("You are probably wondering why i just did that. The answer is idk i was bored.")
+
+let secret = document.createElement(secret);
+
+let secrets = ["___", "_", "___", "__________", "check the developer tools"];
+
+function getRandomIntInclusive(0 , 5) {
+    const minCeiled = Math.ceil(0);
+    const maxFloored = Math.floor(5);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+  }
+  
+for(let i =1; i<11; i++) {
+    let shh = document.createElement(shh);
+    shh.innerText = secrets[getRandomIntInclusive];
+    secret.appendChild(shh);
+}
+
+document.textContent.appendChild(secret)
