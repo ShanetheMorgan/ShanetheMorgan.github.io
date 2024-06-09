bb9533cf7620cd0f1be40bee255fc3d5bb9e3583
ShanetheMorgan <144717727+ShanetheMorgan@users.noreply.github.com>
2024-06-09 23:07:35 +0000

    Finished dynamics

--- Scripts/nothing.js
+++ Scripts/nothing.js
@@ -10,20 +10,20 @@ for(let i = 1; i<100; i++) {
 
 console.log("You are probably wondering why i just did that. The answer is idk i was bored.")
 
-let secret = document.createElement(secret);
+let secret = document.createElement("div");
 
 let secrets = ["___", "_", "___", "__________", "check the developer tools"];
 
-function getRandomIntInclusive(0 , 5) {
-    const minCeiled = Math.ceil(0);
-    const maxFloored = Math.floor(5);
+function getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
   }
   
 for(let i =1; i<11; i++) {
-    let shh = document.createElement(shh);
-    shh.innerText = secrets[getRandomIntInclusive];
+    let shh = document.createElement("p");
+    shh.innerText = secrets[getRandomIntInclusive(0,secrets.length-1)];
     secret.appendChild(shh);
 }
 
-document.textContent.appendChild(secret)
+document.body.appendChild(secret)
